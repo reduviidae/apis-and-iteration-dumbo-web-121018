a079ac0059dedea00e53e14d6fb571b8abe7ff16
Wil Cook <wil.cook.is@gmail.com>
2018-12-11 20:15:11 +0000

    Automatically backed up by Learn

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,10 @@
 def welcome
   # puts out a welcome message here!
+  "Welcome you nerd!"
 end
 
 def get_character_from_user
-  puts "please enter a character name"
+  puts "Please enter a Star Wars character name"
   # use gets to capture the user's input. This method should return that input, downcased.
+  user_input = gets.chomp.downcase
 end
