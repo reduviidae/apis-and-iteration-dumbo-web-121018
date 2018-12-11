7edc22d0ddb48fdcd7b6df2a577cd0428bbc5b82
reduviidae <wiljago@gmail.com>
2018-12-11 12:09:04 -0500

    retrieving films from API based on character successful

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,7 +6,15 @@ def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-
+  characters = response_hash["results"]
+  # binding.pry
+  films = []
+  characters.each do |character|
+    if character["name"] == character_name
+      # binding.pry
+      films << character["films"]
+    end
+  end
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -16,7 +24,11 @@ def get_character_movies_from_api(character_name)
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
+  films
+  binding.pry
 end
+
+get_character_movies_from_api('C-3PO')
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
